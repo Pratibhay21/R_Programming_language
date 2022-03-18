79c67f305a4b45e9351757ca68a75216ad40fedb
Pratibha Yadav <yadavpratibha21111999@gmail.com>
2022-03-19 00:23:56 +0530

    info

---
+++ Summary.R
@@ -0,0 +1,28 @@
+# File:   Summary.R
+# Course: R: An Introduction (with RStudio)
+
+
+########in visualizations,"Pictures first and numbers later
+# INSTALL AND LOAD PACKAGES ################################
+
+library(datasets)  # Load/unload base packages manually
+
+# LOAD DATA ################################################
+
+head(iris)
+
+# SUMMARY() ################################################
+
+summary(iris$Species)       # Categorical variable
+summary(iris$Sepal.Length)  # Quantitative variable
+summary(iris)               # Entire data frame
+
+# CLEAN UP #################################################
+
+# Clear packages
+detach("package:datasets", unload = TRUE)   # For base
+
+# Clear console
+cat("\014")  # ctrl+L
+
+# Clear mind :)
